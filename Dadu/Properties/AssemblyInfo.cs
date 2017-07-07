4484de2496a4c9bf3972e6924206d5529d802e97
Hanafi <muhammad.hanafi.firdaus@gmail.com>
2017-07-07 16:22:55 +0700

    Add project files.

---
+++ Dadu/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Dadu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dadu")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("af1c407b-f75e-4900-999d-99367a21e127")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Dadu/Proses.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dadu
+{
+    class Proses
+    {
+        public void test()
+        {
+            int temp;
+            string a;
+
+            int[] array = new int[5];
+
+            for (int s = 0; s < array.Length; s++)
+            {
+                Console.Write("Masukkan Dadu ke- " + s + " = ");
+                a = Console.ReadLine();
+                array[s] = int.Parse(a);
+            }
+
+            int[] nilai = new int[7];
+            int hasil = 0;
+
+            for (int i = 1; i < 7; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (i == array[j])
+                    {
+                        nilai[i] = nilai[i] + 1;
+                        array[j] = 0;
+                    }
+                }
+            }
+
+            for (int l = 1; l < nilai.Length; l++)
+            {
+                if (l == 1)
+                {
+                    if (nilai[l] > 3)
+                    {
+                        temp = nilai[l] - 3;
+                        hasil = 1000 + (temp * 100);
+                    }
+
+                    if (nilai[l] == 3)
+                    {
+                        hasil = hasil + 1000;
+                    }
+                }
+
+                if (l == 5)
+                {
+                    if (nilai[l] < 3)
+                    {
+                        hasil = hasil + nilai[l] * 50;
+                    }
+
+                    if (nilai[l] >= 3)
+                    {
+                        temp = nilai[l] - 3;
+                        hasil = hasil + 300 + (temp * 50);
+                    }
+                }
+
+                if (nilai[l] >= 3)
+                {
+                    if ((l != 1) && (l != 5))
+                    {
+                        hasil = hasil + (l * 100);
+                    }
+                }
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("hasilnya adalah = " + hasil);
+            Console.ReadKey();
+            Console.Clear();
+
+            test();
+        }
+    }
+}
